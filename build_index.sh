b9a8afc67cfbc9867334118301e84172958929dc
www-Ye <919872507@qq.com>
2023-09-29 14:14:55 +0800

    commit

--- build_index.sh
+++ build_index.sh
@@ -4,6 +4,6 @@ python src/title2fmid.py
 
 python src/build_trie.py
 
-python build_fm_index.py cache/100w_passage_kilt_knowledgesource_full.jsonl cache/kilt_w1002full_corpus.fm_index \
+python src/build_fm_index.py cache/100w_passage_kilt_knowledgesource_full.jsonl cache/kilt_w1002full_corpus.fm_index \
 	--hf_model meta-llama/Llama-2-13b-hf \
 	--format kilt
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python llm2gr.py --dataset hotpotqa --inference_mode recite --model_path meta-llama/Llama-2-13b-hf --isngrams 1 --isretrieval 1 --title_gen_beams 15 --re_beams 10 --lam 0.9 --title_return_nums 2 --gen_len 16
+python src/llm2gr.py --dataset hotpotqa --inference_mode recite --model_path meta-llama/Llama-2-13b-hf --isngrams 1 --isretrieval 1 --title_gen_beams 15 --re_beams 10 --lam 0.9 --title_return_nums 2 --gen_len 16
